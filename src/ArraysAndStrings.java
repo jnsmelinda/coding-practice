464a273068ccb64577d2c0dd40a2258833fe7ee2
jnsmelinda <jns.melinda@gmail.com>
2019-09-28 00:26:04 -0700

    rotate done

--- src/ArraysAndStrings.java
+++ src/ArraysAndStrings.java
@@ -9,11 +9,81 @@ public class ArraysAndStrings {
 //        isPermutationOfPalindrome("tacc     at");
 //        isOneEditAway("abc", "abdc");
 //        System.out.println(compressor("aaabgggcccc"));
-        int[][] arr = {{1,2,3,4},{4,5,6,7}, {7,8,9,10}, {3,3,3,3}};
+        int[][] arr = {{1,2,3},{4,5,6}, {7,8,9}};
         printMatrix(arr);
-        rotate(arr);
+        rotateMatrix(arr);
+//        setZeros(arr);
         System.out.println("---------------------------------");
         printMatrix(arr);
+    }
+
+    public static void rotateMatrix(int[][] matrix){
+        int n = matrix.length;
+        for (int i = 0; i < n/2; i++){
+            for (int j = i; j < n - i - 1; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-j][i];
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+                matrix[j][n-1-i] = temp;
+            }
+        }
+    }
+
+    public static void transpose(int[][] matrix){
+        for (int i = 0; i < matrix.length; i++){
+            for (int j = i; j < matrix.length; j++) {
+                if (matrix[i][j] != matrix[j][i]) {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+                }
+            }
+        }
+    }
+
+    public static boolean isSubstring(String str1, String str2){
+
+
+        return false;
+    }
+
+    public static void setZeros(int[][] matrix){
+        boolean[] row = new boolean[matrix.length];
+        boolean[] col = new boolean[matrix[0].length];
+
+        for (int i = 0; i < matrix.length; i++){
+            for (int j = 0; j < matrix[0].length; j++){
+                if (matrix[i][j] == 0){
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < row.length; i++) {
+            if (row[i]){
+                nullifyRow(matrix, i);
+            }
+        }
+
+        for (int i = 0; i < col.length; i++) {
+            if (col[i]){
+                nullifyColumn(matrix, i);
+            }
+        }
+    }
+
+    private static void nullifyRow(int[][] matrix, int row){
+        for (int i = 0; i < matrix[0].length; i++) {
+            matrix[row][i] = 0;
+        }
+    }
+
+    private static void nullifyColumn(int[][] matrix, int col){
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i][col] = 0;
+        }
     }
 
     public static void printMatrix(int[][] matrix) {
@@ -25,7 +95,6 @@ public class ArraysAndStrings {
                 else if (matrix[i][j] < 10){
                     System.out.println(matrix[i][j] + "   ");
                 }
-
             }
             System.out.println();
         }
@@ -34,7 +103,7 @@ public class ArraysAndStrings {
     public static void rotate(int[][] matrix) {
         int n = matrix.length;
         for (int i = 0; i < n / 2; i++) {
-            for (int j = 0; j < ((n + 1) / 2); j++) {
+            for (int j = i; j < n - 1 - i; j++) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[n - 1 - j][i];
                 matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
