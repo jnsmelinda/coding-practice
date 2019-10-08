ddc9ed4f5595d4584a0ca93ac61725777a6b3896
jnsmelinda <jns.melinda@gmail.com>
2019-10-07 17:29:21 -0700

    sum part B created
    created based on the description of the original excersice not the explanation at the solution section

--- src/likedlist/SumLists.java
+++ src/likedlist/SumLists.java
@@ -2,20 +2,20 @@ package likedlist;
 
 public class SumLists {
 
-    public static LinkedList sumLists(LinkedList list1, LinkedList list2) {
-        int sum = compose(list1.head) + compose(list2.head);
-        System.out.println(sum);
-        LinkedList result = new LinkedList();
+    public static LinkedList sumLists1(LinkedList list1, LinkedList list2) {
+        int sum = compose1(list1.head) + compose1(list2.head);
+        return deCompose1(sum, new LinkedList());
+    }
 
+    private static LinkedList deCompose1(int sum, LinkedList result) {
         while (sum > 0) {
             result.addToTail(sum % 10);
             sum = sum / 10;
         }
-
         return result;
     }
 
-    public static int compose(ListNode current) {
+    public static int compose1(ListNode current) {
         int sum = 0;
         if (current != null) {
             int digit = 1;
@@ -29,6 +29,31 @@ public class SumLists {
         return sum;
     }
 
+    public static LinkedList sumLists2(LinkedList list1, LinkedList list2) {
+        int sum = (compose2(list1.head, list1.size)) + (compose2(list2.head, list2.size));
+        return deCompose2(sum, new LinkedList());
+    }
+
+    public static int compose2(ListNode current, double digit) {
+        int sum = 0;
+        digit = Math.pow(10, digit);
+        while (current != null) {
+            sum += current.data * digit;
+            digit /= 10;
+            current = current.next;
+        }
+
+        return sum;
+    }
+
+    private static LinkedList deCompose2(int sum, LinkedList result) {
+        while (sum > 0) {
+            result.addToFront(sum % 10);
+            sum = sum / 10;
+        }
+        return result;
+    }
+
     private static LinkedList makeList(int[] arr) {
         LinkedList result = new LinkedList();
         for (int element : arr) {
@@ -40,8 +65,9 @@ public class SumLists {
     public static void main(String[] args) {
         int[] arr1 = {7,1,6};
         int[] arr2 = {5,9,2};
-        LinkedList linkedList1 = makeList(arr1);
-        LinkedList linkedList2 = makeList(arr2);
-        System.out.println(sumLists(linkedList1, linkedList2));
+        System.out.println(sumLists1(makeList(arr1), makeList(arr2)));
+        int[] arr3 = {6,1,7};
+        int[] arr4 = {2,9,5};
+        System.out.println(sumLists2(makeList(arr3), makeList(arr4)));
     }
 }
