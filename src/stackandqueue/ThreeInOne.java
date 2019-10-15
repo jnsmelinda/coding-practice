f3ab1cdecbc335e9610dd42e793e39ba9e1ea709
jnsmelinda <jns.melinda@gmail.com>
2019-10-15 15:26:38 -0700

    style cleanup

--- src/stackandqueue/ThreeInOne.java
+++ src/stackandqueue/ThreeInOne.java
@@ -5,17 +5,10 @@ import java.util.List;
 import java.util.Stack;
 
 public class ThreeInOne {
-    private static int stackNum;
     private static int stackCapacity;
-    private static int[] arr;
 
-    public ThreeInOne(int[] arr, int stackNum) {
-        this.stackCapacity = arr.length / stackNum;
-        this.stackNum = stackNum;
-        this.arr = arr;
-    }
-
-    public static List makeStack() {
+    public static List makeStack(int[] arr, int stackNum) {
+        stackCapacity = arr.length / stackNum;
         int counter = 0;
         List list = new ArrayList();
         while (counter < stackNum) {
@@ -26,7 +19,7 @@ public class ThreeInOne {
     }
 
 
-    public static int[] getSubArray(int[] arr, int counter) {
+    private static int[] getSubArray(int[] arr, int counter) {
         int[] subArray = new int[stackCapacity];
         int subArrayIndex = 0;
         for (int i = counter * stackCapacity; i < (counter * stackCapacity) + stackCapacity; i++) {
@@ -37,7 +30,7 @@ public class ThreeInOne {
         return subArray;
     }
 
-    public static Stack addToStack(int[] arr, Stack stack) {
+    private static Stack addToStack(int[] arr, Stack stack) {
         for (int element : arr) {
             stack.push(element);
         }
@@ -47,7 +40,6 @@ public class ThreeInOne {
 
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7,8,9,0};
-        ThreeInOne test = new ThreeInOne(arr, 3);
-        System.out.println(test.makeStack());
+        System.out.println(makeStack(arr, 3));
     }
 }
