a6fdc9759b095c6d6ea622dff8db44889e173f61
jnsmelinda <jns.melinda@gmail.com>
2019-10-24 18:07:43 -0700

    validatetree implemented

--- src/treeandgraph/ValidateBinaryTree.java
+++ src/treeandgraph/ValidateBinaryTree.java
@@ -1,4 +1,28 @@
 package treeandgraph;
 
 public class ValidateBinaryTree {
+
+    public static boolean validateBST(TreeNode root) {
+        return validateBST(root, null, null);
+    }
+
+    private static boolean validateBST(TreeNode root, Integer min, Integer max) {
+        if (root == null) {
+            return true;
+        }
+        if ((min != null && root.data <= min) || (max != null && root.data > max)) {
+            return false;
+        }
+        if (!validateBST(root.left, min, root.data) || !validateBST(root.right, root.data, max)) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 11, 6, 7, 2, 18};
+        BinaryTree tree = new BinaryTree(arr);
+        tree.printSideways();
+        System.out.println(validateBST(tree.root));
+    }
 }
