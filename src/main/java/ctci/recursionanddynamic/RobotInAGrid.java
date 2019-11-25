d864123e99692075617e36585764016b86281b8d
jnsmelinda <jns.melinda@gmail.com>
2019-11-25 15:57:55 -0800

    Update RobotInAGrid.java
    not working

--- src/main/java/ctci/recursionanddynamic/RobotInAGrid.java
+++ src/main/java/ctci/recursionanddynamic/RobotInAGrid.java
@@ -1,99 +1,57 @@
 package ctci.recursionanddynamic;
 
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class RobotInAGrid {
 
-    public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
-        Integer[][] grid = new Integer[m][n];
+    public static ArrayList<Point>[][] getPath(Integer[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        ArrayList<Point>[][] path = new ArrayList[m][n];
 
-        grid[0][0] = 1;
+        Point[][] points = new Point[m][n];
+
+        points[0][0] = new Point(0,0);
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (obstacleGrid[i][j] == 1) {
-                    grid[i][j] = 0;
+                path[i][j] = new ArrayList<>();
+                if (grid[i][j] != null) {
+                    path[i][j].add(null);
                 }
+//                else {
+//                    path[i][i].add(new Point(i,j));
+//                }
+
             }
         }
 
+        System.out.println(Arrays.deepToString(path));
+
         for (int i = 1; i < m; i++) {
-            if (grid[i][0] == null) {
-                grid[i][0] = grid[i-1][0];
+            if (points[i-1][0] == null) {
+                points[i][0] = points[i-1][0];
             }
         }
 
         for (int j = 1; j < n; j++) {
-            if (grid[0][j] == null) {
-                grid[0][j] = grid[0][j-1];
+            if (points[0][j-1] == null) {
+                points[0][j] = points[0][j-1];
             }
         }
+        System.out.println(Arrays.deepToString(points));
 
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (grid[i][j] == null) {
-                    grid[i][j] = grid[i][j-1] + grid[i-1][j];
-                }
-            }
-        }
-        return grid[m-1][n-1];
+
+
+        System.out.println(Arrays.deepToString(points));
+        return path;
     }
 
-//    public static int path(int[][] grid) {
-//        if (grid[0][0] == 1) {
-//            return 0;
-//        }
-//        firstRow(grid[0], grid[0].length + 1);
-//        firstColumn(grid, grid.length + 1);
-//
-//        System.out.println(Arrays.deepToString(grid));
-//        for (int c = 1; c < grid.length; c++) {
-//            for (int r = 1; r < grid[0].length; r++) {
-//                if (grid[c][r] == 1) {
-//                    grid[c][r] = 0;
-//                } else {
-//                    grid[c][r] = grid[c][r - 1] + grid[c - 1][r];
-//                }
-//            }
-//        }
-//
-//        return grid[grid.length-1][grid[0].length - 1];
-//    }
-//
-//    private static void firstColumn(int[][] grid, int obstacle) {
-//        for (int i = 1; i < grid.length; i++) {
-//            if (grid[i][0] == 1) {
-//                obstacle = i;
-//                grid[i][0] = 0;
-//            }
-//            if (i >= obstacle) {
-//                grid[i][0] = 0;
-//            }
-//            else {
-//                grid[i][0] = 1;
-//            }
-//        }
-//    }
-//
-//    private static void firstRow(int[] row, int obstacle) {
-//        for (int i = 0; i < row.length; i++) {
-//            if (row[i] == 1) {
-//                obstacle = i;
-//                row[i] = 0;
-//            }
-//            else if (i >= obstacle) {
-//                row[i] = 0;
-//            }
-//            else {
-//                row[i] = 1;
-//            }
-//        }
-//    }
-
     public static void main(String[] args) {
-        int[][] grid = {{0},{1}};
+        Integer[][] grid = new Integer[3][3];
+        grid[1][1] = 0;
         System.out.println(Arrays.deepToString(grid));
-        System.out.println(uniquePathsWithObstacles(grid));
+        System.out.println(getPath(grid));
     }
 }
