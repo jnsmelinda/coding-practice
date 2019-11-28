75bc53ff88a5c4d8e7c000680f52692324cf949a
jnsmelinda <jns.melinda@gmail.com>
2019-11-27 16:08:51 -0800

    recursive solution added

--- src/main/java/ctci/recursionanddynamic/SubsetOfSets.java
+++ src/main/java/ctci/recursionanddynamic/SubsetOfSets.java
@@ -1,5 +1,6 @@
 package ctci.recursionanddynamic;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class SubsetOfSets {
@@ -8,10 +9,11 @@ public class SubsetOfSets {
         list.add(1);
         list.add(2);
         list.add(3);
-        System.out.println(powerSet(list));
+        System.out.println(powerSetIterative(list));
+        System.out.println(powerSetRecursion(list, 0));
     }
 
-    public static List<List<Integer>> powerSet(List<Integer> list) {
+    public static List<List<Integer>> powerSetIterative(List<Integer> list) {
         List<List<Integer>> result = new ArrayList<>();
         result.add(new ArrayList<>());
         for (Integer item : list) {
@@ -26,5 +28,26 @@ public class SubsetOfSets {
         }
 
         return result;
+    }
+
+    public static ArrayList<ArrayList<Integer>> powerSetRecursion(List<Integer> set, int index) {
+        ArrayList<ArrayList<Integer>> allSubSets;
+        if (set.size() == index) {
+            allSubSets = new ArrayList<>();
+            allSubSets.add(new ArrayList<>());
+        }
+        else {
+            allSubSets = powerSetRecursion(set, index + 1);
+            int item = set.get(index);
+            ArrayList<ArrayList<Integer>> moreSubSets = new ArrayList<>();
+            for (ArrayList<Integer> subSet : allSubSets) {
+                ArrayList<Integer> newSubSets = new ArrayList<>();
+                newSubSets.addAll(subSet);
+                newSubSets.add(item);
+                moreSubSets.add(newSubSets);
+            }
+            allSubSets.addAll(moreSubSets);
+        }
+        return allSubSets;
     }
 }
