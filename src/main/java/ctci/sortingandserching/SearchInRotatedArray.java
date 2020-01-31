e76206ecd27fc56f44d75e19cf535bf1a01d1b03
jnsmelinda <jns.melinda@gmail.com>
2020-01-30 19:58:00 -0800

    Create SearchInRotatedArray.java

---
+++ src/main/java/ctci/sortingandserching/SearchInRotatedArray.java
@@ -0,0 +1,18 @@
+package ctci.sortingandserching;
+
+public class SearchInRotatedArray {
+    public static void main(String[] args) {
+        int[] arr = {15, 16, 20, 25, 27, 30, 1, 2, 4, 10, 11};
+        System.out.println(findLowest(arr));
+    }
+
+    public static int findLowest(int[] arr) {
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+}
