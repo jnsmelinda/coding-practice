9d043f4cb8bd106664c4448690219fa3ff30ac25
jnsmelinda <jns.melinda@gmail.com>
2020-03-02 14:49:13 -0800

    Create MergeBinaryTrees.java

---
+++ src/main/java/leetcode/p617_merge_binary_trees/MergeBinaryTrees.java
@@ -0,0 +1,31 @@
+package leetcode.p617_merge_binary_trees;
+
+public class MergeBinaryTrees {
+    public static void main(String[] args) {
+
+    }
+
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null) {
+            return t2;
+        } else if (t2 == null) {
+            return t1;
+        }
+
+        TreeNode root = new TreeNode(t1.val + t2.val);
+        root.left = mergeTrees(t1.left, t2.left);
+        root.right = mergeTrees(t1.right, t2.right);
+
+        return root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
