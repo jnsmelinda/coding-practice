d643747a1eab60a6596f6ea11ccde470ae1cff4f
jnsmelinda <jns.melinda@gmail.com>
2020-03-03 15:01:52 -0800

    Update Search2DMatrix2.java

---
+++ src/main/java/leetcode/p0240/Search2DMatrix2.java
@@ -0,0 +1,36 @@
+package leetcode.p0240;
+
+import java.util.Arrays;
+
+public class Search2DMatrix2 {
+    public static void main(String[] args) {
+        int[][] matrix = new int[4][4];
+
+        int counter = matrix[0].length * matrix.length - 1;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = matrix[0].length * matrix.length - counter;
+                counter--;
+            }
+        }
+        System.out.println(Arrays.deepToString(matrix));
+        System.out.println(sortedMatrixSearch(matrix, 0));
+    }
+
+    public static boolean sortedMatrixSearch(int[][] matrix, int n) {
+        if (matrix.length > 0) {
+            int row = 0;
+            int col = matrix[0].length - 1;
+            while (row < matrix.length && col >= 0) {
+                if (matrix[row][col] == n) {
+                    return true;
+                } else if (matrix[row][col] > n) {
+                    col--;
+                } else {
+                    row++;
+                }
+            }
+        }
+        return false;
+    }
+}
