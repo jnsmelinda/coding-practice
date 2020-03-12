541fd227ad2bfca96a33f46416a72949eaa7ec52
jnsmelinda <jns.melinda@gmail.com>
2020-03-12 15:15:22 -0700

    new variable to keep the input unchanged

--- src/main/java/leetcode/p1295/find_numbers_with_even_number_of_digits.java
+++ src/main/java/leetcode/p1295/find_numbers_with_even_number_of_digits.java
@@ -6,15 +6,17 @@ public class find_numbers_with_even_number_of_digits {
     public static void main(String[] args) {
         int[] arr = {12,345,2,6,7896};
         System.out.println(findNumbers(arr));
+        System.out.println(find(arr));
     }
 
     public static int findNumbers(int[] nums) {
          int evens = 0;
 
          for (int i = 0; i < nums.length; i++) {
+             int num = nums[i];
              int digits = 0;
-             while(nums[i] > 0) {
-                 nums[i] = nums[i] / 10;
+             while(num > 0) {
+                 num = num / 10;
                  digits++;
              }
              if (digits % 2 == 0) {
