b08a6e231c88cc5a95d683327aef9be5352bd7f3
jnsmelinda <jns.melinda@gmail.com>
2020-03-12 15:06:54 -0700

    Create find_numbers_with_even_number_of_digits.java

---
+++ src/main/java/leetcode/p1295/find_numbers_with_even_number_of_digits.java
@@ -0,0 +1,25 @@
+package leetcode.p1295;
+
+public class find_numbers_with_even_number_of_digits {
+    public static void main(String[] args) {
+        int[] arr = {12,345,2,6,7896};
+        System.out.println(findNumbers(arr));
+    }
+
+    public static int findNumbers(int[] nums) {
+         int evens = 0;
+
+         for (int i = 0; i < nums.length; i++) {
+             int digits = 0;
+             while(nums[i] > 0) {
+                 nums[i] = nums[i] / 10;
+                 digits++;
+             }
+             if (digits % 2 == 0) {
+                 evens++;
+             }
+         }
+
+         return evens;
+    }
+}
