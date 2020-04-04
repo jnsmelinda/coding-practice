899d7f567bc6b8acdaf40e0086d6f3d04a80ad88
jnsmelinda <jns.melinda@gmail.com>
2020-04-04 00:12:21 -0700

    Create SubarraySumEqualsK.java

---
+++ src/main/java/leetcode/p540_subarray_sum_equals_k/SubarraySumEqualsK.java
@@ -0,0 +1,22 @@
+package leetcode.p540_subarray_sum_equals_k;
+
+public class SubarraySumEqualsK {
+    public static void main(String[] args) {
+        int[] arr = {1,2,0,1,4};
+    }
+
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                if (sum == k)
+                    count++;
+            }
+        }
+
+        return count;
+    }
+}
