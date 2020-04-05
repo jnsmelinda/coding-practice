ed6e65cac73a4fe1a2e3ce0040f9587962e0b444
jnsmelinda <jns.melinda@gmail.com>
2020-04-04 22:02:13 -0700

    Update SubarraySumEqualsK.java

--- src/main/java/leetcode/p540_subarray_sum_equals_k/SubarraySumEqualsK.java
+++ src/main/java/leetcode/p540_subarray_sum_equals_k/SubarraySumEqualsK.java
@@ -3,9 +3,10 @@ package leetcode.p540_subarray_sum_equals_k;
 public class SubarraySumEqualsK {
     public static void main(String[] args) {
         int[] arr = {1,2,0,1,4};
+        System.out.println(subArraySum(arr, 2));
     }
 
-    public int subarraySum(int[] nums, int k) {
+    public static int subArraySum(int[] nums, int k) {
         int count = 0;
 
         for (int i = 0; i < nums.length; i++) {
