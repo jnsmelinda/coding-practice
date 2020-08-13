7ca375f60d26d28954e245c25a65c90337db5a72
Melinda Ivanov <jns.melinda@gmail.com>
2020-08-13 16:09:32 -0700

    Create VeryBigSum.java

---
+++ src/main/java/hackerrank/aVeryBigSum/VeryBigSum.java
@@ -0,0 +1,17 @@
+package hackerrank.aVeryBigSum;
+
+public class VeryBigSum {
+    public static void main(String[] args) {
+        long[] ar = {1000000001, 1000000002, 1000000003, 1000000004, 1000000005};
+        System.out.println(aVeryBigSum(ar));
+    }
+
+    public static long aVeryBigSum(long[] ar) {
+        long result = 0;
+        for (int i = 0; i < ar.length; i++) {
+            result += ar[i];
+        }
+
+        return result;
+    }
+}
