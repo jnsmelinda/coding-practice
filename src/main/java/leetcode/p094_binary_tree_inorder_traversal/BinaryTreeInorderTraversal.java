5f7526efc905edd0cf211020bc78af822507c173
Melinda Ivanov <jns.melinda@gmail.com>
2020-08-31 16:08:12 -0700

    Create BinaryTreeInorderTraversal.java

---
+++ src/main/java/leetcode/p094_binary_tree_inorder_traversal/BinaryTreeInorderTraversal.java
@@ -0,0 +1,45 @@
+package leetcode.p094_binary_tree_inorder_traversal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeInorderTraversal {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = null;
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+        System.out.println(inorderTraversal(root));
+    }
+
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        return helper(result, root);
+    }
+
+    public static List<Integer> helper(List<Integer> result, TreeNode root) {
+        if(root == null) {
+            return result;
+        }
+        else {
+            helper(result, root.left);
+            result.add(root.val);
+            helper(result, root.right);
+        }
+
+        return result;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
