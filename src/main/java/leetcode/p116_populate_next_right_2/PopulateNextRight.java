a5b81167b3df1f304d3b305766b3729288c5e854
Melinda Ivanov <jns.melinda@gmail.com>
2020-09-19 18:31:01 -0700

    Create PopulateNextRight.java

---
+++ src/main/java/leetcode/p116_populate_next_right_2/PopulateNextRight.java
@@ -0,0 +1,64 @@
+package leetcode.p116_populate_next_right_2;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class PopulateNextRight {
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        System.out.println(connect(root));
+    }
+
+    public static Node connect(Node root) {
+        if (root == null) {
+            return root;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Node top = queue.poll();
+                if (i < size - 1) {
+                    top.next = queue.peek();
+                }
+
+                if (top.left != null) {
+                    queue.add(top.left);
+                }
+                if (top.right != null) {
+                    queue.add(top.right);
+                }
+            }
+        }
+
+        return root;
+    }
+
+    static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+}
