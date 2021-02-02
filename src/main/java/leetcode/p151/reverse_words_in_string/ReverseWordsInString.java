8679470f144cce0ab8025579a52a1d43b5cbb23b
Melinda Ivanov <jns.melinda@gmail.com>
2021-02-01 18:27:43 -0800

    Create ReverseWordsInString.java

---
+++ src/main/java/leetcode/p151/reverse_words_in_string/ReverseWordsInString.java
@@ -0,0 +1,27 @@
+package leetcode.p151.reverse_words_in_string;
+
+public class ReverseWordsInString {
+    public static void main(String[] args) {
+        System.out.println(reverseWords("  Bob    Loves  Alice   "));
+    }
+
+    public static String reverseWords(String s) {
+        s = s.trim().replaceAll(" +", " ");
+        System.out.println(s);
+        String current = "";
+        StringBuilder reverse = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                reverse.insert(0, " " + current);
+                current = "";
+            }
+            else {
+                current += s.charAt(i);
+            }
+        }
+
+        reverse.insert(0, current);
+
+        return reverse.toString();
+    }
+}
