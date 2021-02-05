405d35b6883478681feeb4561f7b16d9572d8fcb
Melinda Ivanov <jns.melinda@gmail.com>
2021-02-05 14:41:58 -0800

    Create MinimumDistanceBetweenBSTNodes.java

---
+++ src/main/java/leetcode/p0783_minimum_distance_between_bst_nodes/MinimumDistanceBetweenBSTNodes.java
@@ -0,0 +1,58 @@
+package leetcode.p0783_minimum_distance_between_bst_nodes;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MinimumDistanceBetweenBSTNodes {
+    public static void main(String[] args) {
+        TreeNode node = new TreeNode(1);
+        node.right = new TreeNode(3);
+        node.right.left = new TreeNode(2);;
+
+        System.out.println(node);
+        System.out.println(getMinimumDifference(node));
+    }
+
+    public static int getMinimumDifference(TreeNode root) {
+        int min = Integer.MAX_VALUE;
+
+        List<Integer> list = new ArrayList<>();
+        helper(root, list);
+        Collections.sort(list);
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) - list.get(i - 1) < min) {
+                min = list.get(i) - list.get(i - 1);
+            }
+        }
+
+        return min;
+    }
+
+    private static void helper(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        list.add(root.val);
+        helper(root.left, list);
+        helper(root.right, list);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public String toString() {
+            return val + "->" + left + "->" + right;
+        }
+    }
+}
