aff820b35d74697140e35bc8cd847520017c491c
Melinda Ivanov <jns.melinda@gmail.com>
2021-02-12 19:08:53 -0800

    Create NumberOfSegmentsInString.java

---
+++ src/main/java/leetcode/p0434_number_of_segments_in_string/NumberOfSegmentsInString.java
@@ -0,0 +1,28 @@
+package leetcode.p0434_number_of_segments_in_string;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberOfSegmentsInString {
+    public static void main(String[] args) {
+        System.out.println(countSegments("love live! mu'sic forever"));
+    }
+
+    public static int countSegments(String s) {
+        int counter = 0;
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            String str = "";
+            while (i <= s.length() - 1 && s.charAt(i) != ' ') {
+                str += s.charAt(i);
+                i++;
+            }
+            if (!(str.isEmpty())) {
+                list.add(str);
+                str = "";
+            }
+
+        }
+        return list.size();
+    }
+}
