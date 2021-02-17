290b3a267eaa1e1d4adf585fdc16789f83df5181
Melinda Ivanov <jns.melinda@gmail.com>
2021-02-16 16:50:46 -0800

    Create MaxConsecutiveOnes.java

---
+++ src/main/java/leetcode/p485_max_consecutive_ones/MaxConsecutiveOnes.java
@@ -0,0 +1,30 @@
+package leetcode.p485_max_consecutive_ones;
+
+public class MaxConsecutiveOnes {
+    public static void main(String[] args) {
+        int[] arr = {1,0,1,1,1,0,1};
+        System.out.println(findMaxConsecutiveOnes(arr));
+    }
+
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int counter = 0;
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                counter++;
+            }
+            else {
+                if (counter > max) {
+                    max = counter;
+                }
+                counter = 0;
+            }
+        }
+
+        if (counter > max) {
+            return counter;
+        }
+
+        return max;
+    }
+}
