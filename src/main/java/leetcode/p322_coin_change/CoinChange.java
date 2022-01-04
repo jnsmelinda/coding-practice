e0c617b6053cfb7ddd26dc18baba053e4230e551
Melinda Ivanov <jns.melinda@gmail.com>
2022-01-03 16:57:46 -0800

    Update CoinChange.java

--- src/main/java/leetcode/p322_coin_change/CoinChange.java
+++ src/main/java/leetcode/p322_coin_change/CoinChange.java
@@ -4,20 +4,19 @@ import java.util.Arrays;
 
 public class CoinChange {
     public static void main(String[] args) {
-        int[] coins = {3,4,5};
-        System.out.println(coinChange(coins, 2));
+        int[] coins = {1,3,4,5};
+        System.out.println(coinChange(coins, 7));
     }
 
     public static int coinChange(int[] coins, int change) {
+        Arrays.sort(coins);
         int[] dp = new int[change + 1];
         Arrays.fill(dp, change + 1);
         dp[0] = 0;
-        System.out.println(Arrays.toString(dp));
         for (int i = 0; i <= change; i++) {
             for (int j = 0; j < coins.length; j++) {
                 if (coins[j] <= i) {
                     dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
-                    System.out.println(Arrays.toString(dp));
                 }
                 else {
                     break;
