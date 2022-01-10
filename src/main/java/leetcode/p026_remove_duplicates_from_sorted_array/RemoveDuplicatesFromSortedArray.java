f3b5732d66444bbd435b7fb1c72d5c09901b9d28
Melinda Ivanov <jns.melinda@gmail.com>
2022-01-09 16:59:17 -0800

    Create RemoveDuplicatesFromSortedArray.java

---
+++ src/main/java/leetcode/p026_remove_duplicates_from_sorted_array/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,18 @@
+package leetcode.p026_remove_duplicates_from_sorted_array;
+
+public class RemoveDuplicatesFromSortedArray {
+    public static void main(String[] args) {
+        int[] arr = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println(removeDuplicates(arr));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int k = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k++] = nums[i];
+            }
+        }
+        return k;
+    }
+}
